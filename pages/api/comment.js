056c4699b2fad4fc6ce86492a21ca97244b647de
Asad <usasad99@gmail.com>
2022-06-25 09:45:20 +0500

    ? bug fixed

--- pages/api/comment.js
+++ pages/api/comment.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     const { userData: { purchasedProducts } } = await client.request(userPurchasedPrdsQry, { id: req.body.comments[0].userId });
 
-    if (purchasedProducts.includes(req.body.id)) {
+    if (purchasedProducts?.includes(req.body.id)) {
       const result = await client.request(query, req.body);
 
       res.status(200).json(result.updateProduct.comments);
